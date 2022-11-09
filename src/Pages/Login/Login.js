6ec85de4cbdf737078c0a69d66496c106495307f
MD Shahinur Rahman Nayon <mdshahinurrahmannayon@gmail.com>
2022-11-09 15:00:35 +0600

    JWT Localstroge added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -24,7 +24,25 @@ const Login = () => {
         userSignIn(email, password)
         .then(result => {
             const user = result.user;
-            console.log(user);            
+            console.log(user);
+
+            const currentUser = {
+                email: user.email
+            }
+
+            fetch('http://localhost:5000/jwt', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(currentUser)
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                localStorage.setItem('grover-token', data.token)
+            })
+
             navigate(from, { replace: true });
         })
         .catch(error => toast.error(error.message))
